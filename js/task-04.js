272c3d45aefa77bd9ba27662183d653c8ca97016
Inna Tishchenko <innatishchenko29@gmail.com>
2020-07-20 18:02:53 +0300

    added hw-js-03

---
+++ js/task-04.js
@@ -0,0 +1,29 @@
+"use strict";
+const countTotalSalary = function (employees) {
+  let sum = 0;
+  for (const salary of Object.values(employees)) {
+    sum += salary;
+  }
+  return sum;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  })
+); // 400
---
+++ js/task-07.js
@@ -0,0 +1,94 @@
+"use strict";
+
+/*
+ * Типов транзацкий всего два.
+ * Можно положить либо снять деньги со счета.
+ */
+const Transaction = {
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
+};
+
+/*
+ * Каждая транзакция это объект со свойствами: id, type и amount
+ */
+
+const account = {
+  // Текущий баланс счета
+  balance: 0,
+
+  // История транзакций
+  transactions: [],
+
+  /*
+   * Метод создает и возвращает объект транзакции.
+   * Принимает сумму и тип транзакции.
+   */
+  createTransaction(amount, type) {
+    const id = this.transactions.length + 1;
+    const transaction = { id, amount, type };
+    return transaction;
+  },
+
+  /*
+   * Метод отвечающий за добавление суммы к балансу.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций
+   */
+  deposit(amount) {
+    this.transactions.push(this.createTransaction(amount, Transaction.DEPOSIT));
+    this.balance += amount;
+  },
+
+  /*
+   * Метод отвечающий за снятие суммы с баланса.
+   * Принимает сумму танзакции.
+   * Вызывает createTransaction для создания объекта транзакции
+   * после чего добавляет его в историю транзакций.
+   *
+   * Если amount больше чем текущий баланс, выводи сообщение
+   * о том, что снятие такой суммы не возможно, недостаточно средств.
+   */
+  withdraw(amount) {
+    if (amount > balance) {
+      console.log("Транзакция отклонена! Недостаточно средств на счету!");
+      return;
+    } else {
+      this.balance -= amount;
+      this.transactions.push(
+        this.createTransaction(amount, Transaction.WITHDRAW)
+      );
+    }
+  },
+
+  /*
+   * Метод возвращает текущий баланс
+   */
+  getBalance() {
+    return this.balance;
+  },
+
+  /*
+   * Метод ищет и возвращает объект транзации по id
+   */
+  getTransactionDetails(id) {
+    for (const obj of this.transactions)
+      if (obj.id === id) {
+        return obj;
+      }
+  },
+
+  /*
+   * Метод возвращает количество средств
+   * определенного типа транзакции из всей истории транзакций
+   */
+  getTransactionTotal(type) {
+    let totalAmountId = 0;
+    for (const object of this.transactions) {
+      if (object.type === type) {
+        return (totalAmountId += object.amount);
+      }
+    }
+  },
+};
